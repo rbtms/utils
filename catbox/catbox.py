e07fdd65339878bdb0e19f9d1fd92c02afacdcc0
Alvaro Fernandez <nishinishi9999@gmail.com>
2021-11-05 19:14:20 +0100

    Update catbox.py

--- catbox/catbox.py
+++ catbox/catbox.py
@@ -1,34 +1,65 @@
 #
-# Script to upload files and clipboard to catbox
-# Usage: catbox <file>
-#        catbox (if you have an image on your clipboard)
+# Script to upload files and clipboard to catbox/litterbox
+# Usage: catbox.py [litterbox] <file>
+#        catbox.py [litterbox] (if you have an image on your clipboard)
 #
-# Requires: PIL, pyupload (pip), clip.exe (windows)
+# Requires: PIL, requests (pip), clip.exe (windows)
 #
 
 import os
 import sys
 import subprocess
+import requests
 from PIL import ImageGrab
 
-TMP_FILE = "tmp.png"
+API_URL_CATBOX    = "https://catbox.moe/user/api.php"
+API_URL_LITTERBOX = "https://litterbox.catbox.moe/resources/internals/api.php"
+TMP_FILE = "tmp.png" # Not in /tmp because of path issues
 
 def saveClipboardTmp():
     im = ImageGrab.grabclipboard()
     im.save(TMP_FILE, "png")
 
-def uploadFile(path):
-    out = subprocess.check_output("pyupload.exe " + path + " --host=catbox")
-    url = out.split()[-1]
+def uploadUrl(url):
+    return requests.post(url=API_URL_CATBOX, data={
+        "reqtype": "urlupload",
+        "url": url
+    }).text
 
-    return url
+def uploadFile(url, path, data):
+    return requests.post(
+        url=url,
+        files={ "fileToUpload": open(path, "rb") },
+        data=data
+    ).text
+
+def uploadFileToSite(path, site):
+    if site == "CATBOX":
+        return uploadFile(API_URL_CATBOX, path,
+            {"reqtype": "fileupload"}
+        )
+    else:
+        return uploadFile(API_URL_LITTERBOX, path,
+            {"reqtype": "fileupload", "time": "1h"}
+        )
 
 def main():
     path = None
+    site = "CATBOX"
+    uploadType = "FILE"
+
+    # Remove the site so that the first argument is the path of the file
+    if "litterbox" in sys.argv:
+        site = "LITTERBOX"
+        sys.argv.remove("litterbox")
 
     # Use argument path or create temporary file
     if len(sys.argv) > 1:
         path = sys.argv[1]
+
+        if "http" in path:
+            uploadType = "URL"
+
         print("Uploading " + path + ".")
     else:
         saveClipboardTmp()
@@ -36,11 +67,12 @@ def main():
         print("Uploading clipboard.")
 
     # Bytes object
-    url = uploadFile(path)
+    url = uploadFileToSite(path, site) if uploadType == "FILE" else uploadUrl(path)
+    print(url)
 
     # Copy to clipboard
-    subprocess.run("clip.exe", input=url)
-    print(url.decode("utf-8") + " pasted to clipboard.")
+    subprocess.run("clip.exe", input=bytes(url, "utf-8"))
+    print("[" + url + "] pasted to clipboard.")
 
     # Remove temporary file
     if path == TMP_FILE:
